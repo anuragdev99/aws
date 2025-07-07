ef6e35e6b9499a36907f6d83173495c63bd4bb3b
anuragdev99 <anuragdevops99@gmail.com>
2025-07-07 19:42:37 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "awsanurag99-tfstate"
     key            = "terraform/state"
-    region         = "eu-east-1"
+    region         = "us-east-1"
     encrypt        = true
   }
 }
