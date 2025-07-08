e0888144d3016a0044ca9bca3477d81389e653e0
anuragdev99 <anuragdevops99@gmail.com>
2025-07-08 09:52:49 +0530

    Update remote_exec.tf

--- remote_exec.tf
+++ remote_exec.tf
@@ -1,7 +1,7 @@
 # remote_exec.tf
 
 resource "null_resource" "format_windows_disks" {
-  # Re-run formatting whenever the volume IDs change
+  # Trigger re-provisioning when the attached volume IDs change
   triggers = {
     volume_ids = join(",", aws_ebs_volume.data_volume[*].id)
   }
@@ -14,19 +14,16 @@ resource "null_resource" "format_windows_disks" {
       user     = "Administrator"
       password = var.admin_password
 
-      https    = false   # use HTTP
-      insecure = true    # allow unencrypted Basic auth
+      https    = false    # HTTP
+      insecure = true     # allow unencrypted Basic auth
     }
 
     inline = [
-      # give Windows time to detect and expose the new disks
       "Start-Sleep -Seconds 30",
-
-      # find all RAW disks, init MBR, create a single partition, assign a drive letter, then format NTFS
       "Get-Disk | Where PartitionStyle -Eq 'RAW' | ForEach-Object {",
       "  Initialize-Disk -Number $_.Number -PartitionStyle MBR",
       "  $p = New-Partition -DiskNumber $_.Number -UseMaximumSize -AssignDriveLetter",
-      "  Format-Volume -Partition $p -FileSystem NTFS -NewFileSystemLabel DataDisk -Confirm:$false",
+      "  Format-Volume -Partition $p -FileSystem NTFS -Confirm:$false",
       "}"
     ]
   }
