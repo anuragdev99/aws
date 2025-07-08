9d1e32193e22ae21bf2aae60760ac1cbfeb2909d
anuragdev99 <anuragdevops99@gmail.com>
2025-07-08 09:14:52 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -13,6 +13,20 @@ output "vm1_availability_zone" {
   value       = aws_instance.vm1.availability_zone
 }
 
+#####Volume########
+output "data_volume_ids" {
+  description = "IDs of the two 1 GiB volumes attached to vm1"
+  value       = aws_ebs_volume.data_volume[*].id
+}
+
+output "data_volume_devices" {
+  description = "Device names as seen by the OS"
+  value       = aws_volume_attachment.attach[*].device_name
+}
+
+
+
+#VM2#####################################################
 output "vm2_instance_id" {
   description = "The ID of the second EC2 instance (vm2)"
   value       = aws_instance.vm2.id
