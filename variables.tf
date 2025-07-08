0b06f7519a962958337d254b4164aa62c991a1e0
anuragdev99 <anuragdevops99@gmail.com>
2025-07-08 22:38:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,5 +2,5 @@ variable "admin_password" {
   description = "Administrator password for vm1 (used by WinRM)"
   type        = string
   sensitive   = true
-  default     = "gfBpn)-O66P9.4Mgt8zKk(VAJmg)w(dV"
+  default     = "f?Idwm3?9M)76@u3lMXLDhHLNGsJYaB?"
 }
