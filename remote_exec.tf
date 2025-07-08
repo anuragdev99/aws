66ac61ac25188fd58a7e6703f6441d80d905253c
anuragdev99 <anuragdevops99@gmail.com>
2025-07-08 10:05:36 +0530

    Update remote_exec.tf

--- remote_exec.tf
+++ remote_exec.tf
@@ -1,11 +1,18 @@
 # remote_exec.tf
 
 resource "null_resource" "format_windows_disks" {
-  # re-trigger when volume IDs change
+  # re-trigger when the EBS volume IDs change
   triggers = {
     volume_ids = join(",", aws_ebs_volume.data_volume[*].id)
   }
 
+  # 1) copy the PowerShell script to the VM
+  provisioner "file" {
+    source      = "${path.module}/scripts/format_disks.ps1"
+    destination = "C:/Windows/Temp/format_disks.ps1"
+  }
+
+  # 2) execute it under PowerShell
   provisioner "remote-exec" {
     connection {
       type     = "winrm"
@@ -14,16 +21,13 @@ resource "null_resource" "format_windows_disks" {
       user     = "Administrator"
       password = var.admin_password
 
-      https    = false
-      insecure = true
+      https    = false   # HTTP
+      insecure = true    # allow unencrypted/basic auth
     }
 
+    # Call the script file
     inline = [
-      # pause so Windows discovers the new disks
-      "powershell -NoProfile -NonInteractive -ExecutionPolicy Bypass -Command \"Start-Sleep -Seconds 30\"",
-
-      # find all RAW disks, initialize, partition, assign drive letters and format
-      "powershell -NoProfile -NonInteractive -ExecutionPolicy Bypass -Command \"Get-Disk | Where-Object PartitionStyle -Eq 'RAW' | ForEach-Object { Initialize-Disk -Number $_.Number -PartitionStyle MBR; $p = New-Partition -DiskNumber $_.Number -UseMaximumSize -AssignDriveLetter; Format-Volume -Partition $p -FileSystem NTFS -Confirm:`$false }\""
+      "powershell -NoProfile -NonInteractive -ExecutionPolicy Bypass -File C:/Windows/Temp/format_disks.ps1"
     ]
   }
 }
