00891f555f97d280bb7beff055c24276c9a58a05
anuragdev99 <anuragdevops99@gmail.com>
2025-07-08 08:32:40 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,24 @@
-output "instance_id" {
-  value = aws_instance.vm.id
+output "vm1_instance_id" {
+  description = "The ID of the first EC2 instance (vm1)"
+  value       = aws_instance.vm1.id
 }
 
-output "public_ip" {
-  value = aws_instance.vm.public_ip
+output "vm1_public_ip" {
+  description = "The public IP of the first EC2 instance (vm1)"
+  value       = aws_instance.vm1.public_ip
+}
+
+output "vm1_availability_zone" {
+  description = "The availability zone of vm1 (useful for attaching EBS volumes later)"
+  value       = aws_instance.vm1.availability_zone
+}
+
+output "vm2_instance_id" {
+  description = "The ID of the second EC2 instance (vm2)"
+  value       = aws_instance.vm2.id
+}
+
+output "vm2_public_ip" {
+  description = "The public IP of the second EC2 instance (vm2)"
+  value       = aws_instance.vm2.public_ip
 }
