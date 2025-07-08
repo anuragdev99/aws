6955034196a1bad666d651c21f4c1cc44e35f42c
anuragdev99 <anuragdevops99@gmail.com>
2025-07-08 10:18:38 +0530

    Update remote_exec.tf

--- remote_exec.tf
+++ remote_exec.tf
@@ -1,30 +1,27 @@
-# remote_exec.tf
-
 resource "null_resource" "format_windows_disks" {
-  # Re-run formatting whenever the volume IDs change
+  # re-run when EBS volume IDs **or** script contents change
   triggers = {
     volume_ids = join(",", aws_ebs_volume.data_volume[*].id)
+    script_md5 = filesha256("${path.module}/scripts/format_disks.ps1")
   }
 
-  # 1. Connection block applies to all provisioners in this resource
   connection {
     type     = "winrm"
     host     = aws_instance.vm1.public_ip
     port     = 5985
     user     = "Administrator"
     password = var.admin_password
-
     https    = false
     insecure = true
   }
 
-  # 2. Copy the PowerShell script to the VM
+  # 1) copy updated script
   provisioner "file" {
     source      = "${path.module}/scripts/format_disks.ps1"
     destination = "C:/Windows/Temp/format_disks.ps1"
   }
 
-  # 3. Execute the script under PowerShell
+  # 2) execute under PowerShell
   provisioner "remote-exec" {
     inline = [
       "powershell -NoProfile -NonInteractive -ExecutionPolicy Bypass -File C:/Windows/Temp/format_disks.ps1"
