9f45033f7b830de1d05e6880908e37f3722d6ce0
anuragdev99 <anuragdevops99@gmail.com>
2025-07-08 08:25:40 +0530

    Update new-vm.tf

--- new-vm.tf
+++ new-vm.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "vm" {
+resource "aws_instance" "vm1" {
   ami                         = "ami-0ed9f8d63c9e8b95a"
   instance_type               = "t2.micro"
   subnet_id                   = "subnet-0e09b359f12239236"
@@ -7,7 +7,20 @@ resource "aws_instance" "vm" {
   associate_public_ip_address = true
 
   tags = {
-    Name = "Terraform-EC2"
+    Name = "my-vm1"
+  }
+}
+
+resource "aws_instance" "vm2" {
+  ami                         = "ami-0ed9f8d63c9e8b95a"
+  instance_type               = "t2.micro"
+  subnet_id                   = "subnet-0e09b359f12239236"
+  vpc_security_group_ids      = ["sg-0e2fe4c52772a7b26"]
+  key_name                    = "my-newkey2025"
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "my-vm2"
   }
 }
 
