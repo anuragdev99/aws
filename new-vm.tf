2fe53fbceb0e930ac4466e71ea1d5dee1371bcbc
anuragdev99 <anuragdevops99@gmail.com>
2025-07-30 00:02:13 +0530

    Update new-vm.tf

--- new-vm.tf
+++ new-vm.tf
@@ -6,18 +6,26 @@ resource "aws_instance" "vm1" {
   key_name                    = "my-newkey2025"
   associate_public_ip_address = true
 
-  user_data = <<-EOF
-    <powershell>
-    net user Administrator "Password@@2025##"
-    winrm quickconfig -force
-    winrm set winrm/config/service @{AllowUnencrypted="true"}
-    winrm set winrm/config/service/auth @{Basic="true"}
-    Set-Item -Path WSMan:\\localhost\\Service\\AllowUnencrypted -Value $true
-    Set-Item -Path WSMan:\\localhost\\Service\\Auth\\Basic -Value $true
-    Enable-PSRemoting -Force
-    New-NetFirewallRule -Name "WinRM-HTTP" -DisplayName "Allow WinRM over HTTP" -Protocol TCP -LocalPort 5985 -Action Allow
-    </powershell>
-  EOF
+user_data = <<-EOF
+  <powershell>
+  net user Administrator "Password@@2025##"
+  winrm quickconfig -force
+
+  # Server-side settings
+  winrm set winrm/config/service @{AllowUnencrypted="true"}
+  winrm set winrm/config/service/auth @{Basic="true"}
+  Set-Item -Path WSMan:\\localhost\\Service\\AllowUnencrypted -Value $true
+  Set-Item -Path WSMan:\\localhost\\Service\\Auth\\Basic -Value $true
+
+  # Client-side settings (required for WinRM Basic)
+  winrm set winrm/config/client/auth @{Basic="true"}
+  Set-Item -Path WSMan:\\localhost\\Client\\Auth\\Basic -Value $true
+
+  Enable-PSRemoting -Force
+  New-NetFirewallRule -Name "WinRM-HTTP" -DisplayName "Allow WinRM over HTTP" -Protocol TCP -LocalPort 5985 -Action Allow
+  </powershell>
+ EOF
+
 
   tags = {
     Name = "my-vm1"
