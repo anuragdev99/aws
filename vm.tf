32eb4b9f518858238247f56df978aa61b5de19aa
anuragdev99 <anuragdevops99@gmail.com>
2025-07-30 21:51:01 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -35,7 +35,7 @@ resource "aws_instance" "vm1" {
   associate_public_ip_address = true
 
   tags = {
-    Name = "ssm-windows-vm"
+    Name = "ssm-windows-vm1"
   }
 }
 
@@ -63,4 +63,18 @@ resource "aws_ssm_document" "format_data_disks" {
       }
     ]
   })
+}
+
+resource "aws_instance" "vm2" {
+  ami                         = "ami-0758218dcb57e4a14" # Make sure it's SSM-enabled
+  instance_type               = "t2.micro"
+  subnet_id                   = "subnet-0e09b359f12239236"
+  vpc_security_group_ids      = ["sg-0e2fe4c52772a7b26"]
+  key_name                    = "my-newkey2025"
+  iam_instance_profile        = aws_iam_instance_profile.ssm_profile.name
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "ssm-windows-vm2"
+  }
 }
