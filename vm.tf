60e0555bdb4533c0e769c0c686099ff03b8dd80e
anuragdev99 <anuragdevops99@gmail.com>
2025-07-30 22:00:53 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -3,15 +3,13 @@ resource "aws_iam_role" "ssm_role" {
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
   })
 }
 
@@ -26,7 +24,7 @@ resource "aws_iam_instance_profile" "ssm_profile" {
 }
 
 resource "aws_instance" "vm1" {
-  ami                         = "ami-0758218dcb57e4a14" # Make sure it's SSM-enabled
+  ami                         = "ami-0758218dcb57e4a14" # Ensure this is SSM-enabled
   instance_type               = "t2.micro"
   subnet_id                   = "subnet-0e09b359f12239236"
   vpc_security_group_ids      = ["sg-0e2fe4c52772a7b26"]
@@ -39,34 +37,8 @@ resource "aws_instance" "vm1" {
   }
 }
 
-resource "aws_ssm_document" "format_data_disks" {
-  name          = "FormatDataDisks"
-  document_type = "Command"
-  content = jsonencode({
-    schemaVersion = "2.2",
-    description   = "Format newly attached disks on Windows",
-    mainSteps     = [
-      {
-        action = "aws:runPowerShellScript",
-        name   = "formatRawDisks",
-        inputs = {
-          runCommand = [
-            "$rawDisks = Get-Disk | Where-Object PartitionStyle -Eq 'RAW'",
-            "foreach ($disk in $rawDisks) {",
-            "  $partition = Initialize-Disk -Number $disk.Number -PartitionStyle MBR -PassThru |",
-            "               New-Partition -UseMaximumSize -AssignDriveLetter",
-            "  Format-Volume -Partition $partition -FileSystem NTFS -Confirm:$false",
-            "}",
-            "Write-Output 'Disk formatting complete.' > C:\\Windows\\Temp\\disk_format_log.txt"
-          ]
-        }
-      }
-    ]
-  })
-}
-
 resource "aws_instance" "vm2" {
-  ami                         = "ami-0758218dcb57e4a14" # Make sure it's SSM-enabled
+  ami                         = "ami-0758218dcb57e4a14"
   instance_type               = "t2.micro"
   subnet_id                   = "subnet-0e09b359f12239236"
   vpc_security_group_ids      = ["sg-0e2fe4c52772a7b26"]
@@ -77,4 +49,41 @@ resource "aws_instance" "vm2" {
   tags = {
     Name = "ssm-windows-vm2"
   }
+}
+
+resource "aws_ssm_document" "format_data_disks" {
+  name          = "FormatDataDisks"
+  document_type = "Command"
+
+  content = jsonencode({
+    schemaVersion = "2.2",
+    description   = "Format newly attached disks on Windows",
+    mainSteps     = [
+      {
+        action = "aws:runPowerShellScript",
+        name   = "formatRawDisks",
+        inputs = {
+          runCommand = [
+            "try {",
+            "  $rawDisks = Get-Disk | Where-Object PartitionStyle -Eq 'RAW'",
+            "  foreach ($disk in $rawDisks) {",
+            "    $partition = Initialize-Disk -Number $disk.Number -PartitionStyle MBR -PassThru |",
+            "                 New-Partition -UseMaximumSize -AssignDriveLetter",
+            "    Format-Volume -Partition $partition -FileSystem NTFS -Force -Confirm:$false",
+            "  }",
+            "  'Disk formatting complete.' | Out-File 'C:\\Windows\\Temp\\disk_format_log.txt'",
+            "} catch {",
+            "  'Disk formatting failed: ' + $_ | Out-File 'C:\\Windows\\Temp\\disk_format_error_log.txt'",
+            "}"
+          ]
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_ssm_association" "vm1_disk_format" {
+  name                = aws_ssm_document.format_data_disks.name
+  instance_id         = aws_instance.vm1.id
+  wait_for_success_timeout = "5m"
 }
