2924237b95abe356f56b37d5ef9c3d4d1b8b64d0
anuragdev99 <anuragdevops99@gmail.com>
2025-07-30 21:33:11 +0530

    Create vm.tf

---
+++ vm.tf
@@ -0,0 +1,62 @@
+resource "aws_iam_role" "ssm_role" {
+  name = "ssm-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ssm_profile" {
+  name = "ssm-profile"
+  role = aws_iam_role.ssm_role.name
+}
+
+resource "aws_instance" "vm1" {
+  ami                         = "ami-0758218dcb57e4a14" # Make sure it's SSM-enabled
+  instance_type               = "t2.micro"
+  subnet_id                   = "subnet-0e09b359f12239236"
+  vpc_security_group_ids      = ["sg-0e2fe4c52772a7b26"]
+  key_name                    = "my-newkey2025"
+  iam_instance_profile        = aws_iam_instance_profile.ssm_profile.name
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "ssm-windows-vm"
+  }
+}
+
+resource "aws_ssm_document" "format_disks_doc" {
+  name          = "FormatWindowsDisks"
+  document_type = "Command"
+  content = jsonencode({
+    schemaVersion = "2.2",
+    description   = "Format raw disks and assign drive letters",
+    mainSteps     = [
+      {
+        action = "aws:runPowerShellScript",
+        name   = "formatDisks",
+        inputs = {
+          runCommand = [
+            "Write-Output 'Starting disk format...'",
+            "Get-Disk | Where-Object PartitionStyle -Eq 'RAW' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -UseMaximumSize -AssignDriveLetter | Format-Volume -FileSystem NTFS -Confirm:\$false",
+            "Write-Output 'Disks formatted successfully!' > C:\\Windows\\Temp\\format_log.txt"
+          ]
+        }
+      }
+    ]
+  })
+}
